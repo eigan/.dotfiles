e9b83bfbeb48f04560d92412092734890d162fcf
Einar Gangsø <mail@einargangso.no>
2019-01-15 23:47:39 +0100

    add tt check

---
+++ polybar/bin/polybar-tt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ ! -f /tmp/polybar-tt-last ]
+then
+    touch -d "5 days ago" /tmp/polybar-tt-last
+fi
+
+if [ ! -f /tmp/polybar-tt-cache ]
+then
+    touch /tmp/polybar-tt-cache
+fi
+
+last=$(echo $(($(date +%s) - $(date +%s -r /tmp/polybar-tt-last))))
+
+treshold=3600
+cache=/tmp/polybar-tt-cache
+
+if [ $treshold -ge "$last" ] && [ ! -f /tmp/polybar-tt-now ]
+then
+    cat $cache
+else
+    touch /tmp/polybar-tt-last
+    rm -f /tmp/polybar-tt-now
+
+    tt=$(tt status --clear-cache --oneline | tail -1)
+
+    echo "  $tt" > $cache
+    echo "  $tt"
+fi
