801cb3b1d09532aacbdd1c66649611b8be84de5c
Einar Gangsø <mail@einargangso.no>
2021-05-17 15:03:53 +0200

    support pipewire on toggle thing

--- base/bin/mic-state.sh
+++ base/bin/mic-state.sh
@@ -1,5 +1,7 @@
-if pulseaudio-ctl | grep "Is source muted" | grep -q "yes"; then
-  echo "{\"text\": \"\", \"class\": \"mic-muted\", \"alt\": \"ok\"}"
+CURRENT_SOURCE=$(pactl info | grep "Default Source" | cut -f3 -d" ")
+
+if [[ $(pactl list sources | grep -A 10 $CURRENT_SOURCE | grep "Mute: yes") ]]; then
+    echo "{\"text\": \"\", \"class\": \"mic-muted\", \"alt\": \"ok\"}"
 else
-  echo "{\"text\": \"\", \"class\": \"mic-unmuted\", \"alt\": \"ok\"}"
+    echo "{\"text\": \"\", \"class\": \"mic-unmuted\", \"alt\": \"ok\"}"
 fi
