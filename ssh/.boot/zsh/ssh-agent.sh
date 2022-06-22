2ebb664826f0cf9b1b5c114b82b786b8e702e1c2
Einar Gangsø <mail@einargangso.no>
2022-06-22 06:46:00 +0200

    Indentions

--- ssh/.boot/zsh/ssh-agent.sh
+++ ssh/.boot/zsh/ssh-agent.sh
@@ -4,21 +4,20 @@ source ~/.config/shell/env
 ssh_agent_keys=$SSH_AGENT_KEYS
 
 if [ -f ~/.ssh/agent.env ] ; then
-    . ~/.ssh/agent.env > /dev/null
-    if ! kill -0 $SSH_AGENT_PID > /dev/null 2>&1; then
-        echo "Stale agent file found. Spawning new agent… "
-        eval `ssh-agent | tee ~/.ssh/agent.env`
-
-		for i in ${ssh_agent_keys[@]}; do
-			ssh-add $i
-		done
-    fi
-else
-    echo "Starting ssh-agent"
+  . ~/.ssh/agent.env > /dev/null
+  if ! kill -0 $SSH_AGENT_PID > /dev/null 2>&1; then
+    echo "Stale agent file found. Spawning new agent… "
     eval `ssh-agent | tee ~/.ssh/agent.env`
-    
-	for i in ${ssh_agent_keys[@]}; do
-		ssh-add $i
-	done
-		
+
+    for i in ${ssh_agent_keys[@]}; do
+      ssh-add $i
+    done
+  fi
+else
+  echo "Starting ssh-agent"
+  eval `ssh-agent | tee ~/.ssh/agent.env`
+
+  for i in ${ssh_agent_keys[@]}; do
+    ssh-add $i
+  done
 fi
